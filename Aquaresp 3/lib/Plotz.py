906af24889a1c8f3512d1c2d07e1768980399c55
Morten Bo Svendsen <mortenbosvendsen@gmail.com>
2018-12-13 15:49:07 +0100

    Changed marker size

--- Aquaresp 3/lib/Plotz.py
+++ Aquaresp 3/lib/Plotz.py
@@ -44,7 +44,8 @@ for resp in range(1,filehandling.GetNumResp()+1):
 	p = figure(title="Oxygen Consumption: Mass-Specific ", x_axis_label='Time [h]', y_axis_label='MO2 [mgO2 / kg / hr]',tools="box_zoom,wheel_zoom,save,hover,reset,crosshair,",tooltips="@y",x_range=(0, np.array(x).max() +5), y_range=(np.array(y).min() -20, np.array(y).max() +20))
 	p.toolbar.active_scroll = p.select_one(WheelZoomTool) 
 	# p.scatter(x, y, legend="Resp 1", radius=.2,fill_color=(0,250,0), fill_alpha=0.6,line_color=(0,0,0))
-	p.scatter(x, y, legend="Resp 1", radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	# p.scatter(x, y, legend="Resp 1", radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	p.circle(x, y, legend="Resp " + str(resp), size =7,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
 	p.line(x, y, alpha=0.6,line_color=(0,0,0))
 	# show the results
 	save(p)
@@ -59,7 +60,10 @@ for resp in range(1,filehandling.GetNumResp()+1):
 	p = figure(title="Oxygen Consumption: Absolute ", x_axis_label='Time [h]', y_axis_label='MO2 [mgO2 / hr]',tools="box_zoom,wheel_zoom,save,hover,reset,crosshair,",tooltips="@y",x_range=(0, np.array(x).max() +5), y_range=(np.array(y).min(), np.array(y).max()))
 	p.toolbar.active_scroll = p.select_one(WheelZoomTool) 
 	# p.scatter(x, y, legend="Resp 1", radius=.2,fill_color=(0,250,0), fill_alpha=0.6,line_color=(0,0,0))
-	p.scatter(x, y, legend="Resp 1", radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	# p.scatter(x, y, legend="Resp 1", radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	p.circle(x, y, legend="Resp " + str(resp), size =7,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	
+	
 	p.line(x, y, alpha=0.6,line_color=(0,0,0))
 	# show the results
 	save(p)
@@ -81,7 +85,9 @@ for resp in range(1,filehandling.GetNumResp()+1):
 	output_file(myp + "MO2_all.html",mode="inline")
 
 	p.toolbar.active_scroll = p.select_one(WheelZoomTool) 
-	p.scatter(x, y, legend="Resp " + str(resp), radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	# p.scatter(x, y, legend="Resp " + str(resp), radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	p.circle(x, y, legend="Resp " + str(resp), size =7,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	
 	p.line(x, y, alpha=0.6,line_color=(0,0,0))
 	# show the results
 save(p)
@@ -105,7 +111,8 @@ for resp in range(1,filehandling.GetNumResp()+1):
 	output_file(myp + "MO2_ABS_all.html",mode="inline")
 
 	p.toolbar.active_scroll = p.select_one(WheelZoomTool) 
-	p.scatter(x, y, legend="Resp " + str(resp), radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	# p.scatter(x, y, legend="Resp " + str(resp), radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	p.circle(x, y, legend="Resp " + str(resp), size =7,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
 	p.line(x, y, alpha=0.6,line_color=(0,0,0))
 	# show the results
 save(p)
@@ -137,7 +144,8 @@ for resp in range(1,filehandling.GetNumResp()+1):
 	output_file(myp + "MO2_PO2.html",mode="inline")
 
 	p.toolbar.active_scroll = p.select_one(WheelZoomTool) 
-	p.scatter(x, y, legend="Resp " + str(resp), radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	# p.scatter(x, y, legend="Resp " + str(resp), radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	p.circle(x, y, legend="Resp " + str(resp), size =7,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
 	p.line(x, y, alpha=0.6,line_color=(0,0,0))
 	# show the results
 save(p)
@@ -166,7 +174,8 @@ for resp in range(1,filehandling.GetNumResp()+1):
 	output_file(myp + "MO2_ABS_PO2.html",mode="inline")
 
 	p.toolbar.active_scroll = p.select_one(WheelZoomTool) 
-	p.scatter(x, y, legend="Resp " + str(resp), radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	# p.scatter(x, y, legend="Resp " + str(resp), radius=.2,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
+	p.circle(x, y, legend="Resp " + str(resp), size =7,fill_color=color[resp-1], fill_alpha=0.6,line_color=(0,0,0))
 	p.line(x, y, alpha=0.6,line_color=(0,0,0))
 	# show the results
 save(p)
