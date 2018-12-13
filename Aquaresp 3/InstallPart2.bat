f6b713af2aa5578ce1f407f1826612f434190556
Morten Bo Svendsen <mortenbosvendsen@gmail.com>
2018-12-13 14:58:35 +0100

    Name change and minor edits
    Edited installpart2.bat to ensure that the installation file is run in the correct directory

--- Aquaresp 3/InstallPart2.bat
+++ Aquaresp 3/InstallPart2.bat
@@ -1,4 +1,8 @@
-InstallPython.exe InstallAllUsers=0 PrependPath=1 Include_test=0 
+SET mypath=%~dp0
+echo %mypath:~0,-1%
+cd mypath
+InstallPython.exe InstallAllUsers=0 PrependPath=1 Include_test=0
+
 
 python -m pip install --upgrade pip
 python -m pip install bokeh
