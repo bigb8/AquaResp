7feb237409f07e5efc0b1d1dedf1da64b0c88859
Morten Bo Svendsen <mortenbosvendsen@gmail.com>
2019-02-28 17:09:36 +0100

    Installation step 2 is now two-part
    Installation step is now devided in two, as it was otherwise not working for all users

---
+++ Aquaresp 3/InstallPart2b.bat
@@ -0,0 +1,8 @@
+python -m pip install --upgrade pip
+python -m pip install bokeh
+python -m pip install numpy
+python -m pip install scipy
+python -m pip install matplotlib
+python -m pip install -U wxPython
+python -m pip install mcculw
+pause
