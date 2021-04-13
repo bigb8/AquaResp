c03f4cf8c2a87df48394b6f4f706723a4e2c787d
Morten Bo Svendsen <mortenbosvendsen@gmail.com>
2021-04-13 10:56:35 +0200

    Old files deleted

--- Aquaresp 3/InstallPart2a.bat
+++
@@ -1,5 +0,0 @@
-SET mypath=%~dp0
-echo %mypath:~0,-1%
-cd %mypath
-InstallPython.exe InstallAllUsers=0 PrependPath=1 Include_test=0
-pause
--- Aquaresp 3/InstallPart2b.bat
+++
@@ -1,8 +0,0 @@
-python -m pip install --upgrade pip
-python -m pip install bokeh==1.3.0
-python -m pip install numpy
-python -m pip install scipy
-python -m pip install matplotlib
-python -m pip install -U wxPython
-python -m pip install mcculw
-pause
